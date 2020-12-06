7be20a3d5f2eef94e7b137800f391d96e683081c
Aokashi <Aokashi.Towa@Gmail.com>
2020-12-06 19:30:34 +0900

    chore: getCategoryUrl にコメントを追加

--- src/utils/getCategoryUrl.ts
+++ src/utils/getCategoryUrl.ts
@@ -1,3 +1,8 @@
+/**
+ * 指定したカテゴリに含まれる記事一覧の絶対パスを出力します。
+ * @param category カテゴリ名
+ * @returns category に該当する記事一覧の絶対パス (URLが必要な場合は別途サイトの URL と併用してください)
+ */
 const getCategoryUrl = (category: string) => {
     return `/category/${category}`
 }
