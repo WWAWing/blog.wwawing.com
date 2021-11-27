f87a2b165e4500c0aeedbfa90a75224aabaefe7d
Aokashi <Aokashi.Towa@Gmail.com>
2021-11-27 13:45:49 +0900

    bio の画像を StaticImage に置き換え

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -7,18 +7,13 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { StaticImage } from "gatsby-plugin-image"
 
 import { rhythm } from "../utils/typography"
 
 const Bio: React.FC = () => {
   const data: GatsbyTypes.BioQueryQuery = useStaticQuery<GatsbyTypes.BioQueryQuery>(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
-        childImageSharp {
-          gatsbyImageData(layout: FIXED)
-        }
-      }
       site {
         siteMetadata {
           author {
@@ -41,19 +36,18 @@ const Bio: React.FC = () => {
         marginBottom: rhythm(2),
       }}
     >
-      <GatsbyImage
-        image={data.avatar.childImageSharp.gatsbyImageData}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
+      <div style={{
+        marginRight: rhythm(1 / 2),
+        marginBottom: 0,
+        minWidth: 50,
+      }}>
+        <StaticImage
+          src="../../content/assets/profile-pic.png"
+          alt={author.name}
+          width={50}
+          height={50}
+        />
+      </div>
       <div>
         <p
           style={{
